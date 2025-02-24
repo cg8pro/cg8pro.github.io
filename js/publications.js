17cdf27f745d97388afc30d11759ed82d6e559cd
Dva R <cg8>
2025-02-24 22:07:08 +0800

    初始化publictions.js

--- js/publications.js
+++ js/publications.js
@@ -1,50 +1,8 @@
 var publications = [
     {
         title: '2025-1',
-        description: '探索世界奇观，珍藏自然之美',
-        imageSrc: '../img/pictorial-cover/index_midj_artists.jpg',
-        url: ''
-    },
-    {
-        title: '《艺术观察》特刊：当代艺术新趋势',
-        description: '深度解析全球艺术市场动态',
-        imageSrc: '../img/chatbg.png',
-        url: ''
-    },
-    {
-        title: '《科技前沿》2025：人工智能革命',
-        description: '揭秘AI技术如何改变未来生活',
-        imageSrc: '../img/index_midj_artists.jpg',
-        url: ''
-    },
-    {
-        title: '《旅行者》年度特辑：极地探险指南',
-        description: '挑战地球最寒冷角落的生存法则',
-        imageSrc: '../img/chatbg.png',
-        url: ''
-    },
-    {
-        title: '《美食地图》：亚洲街头小吃巡礼',
-        description: '从东京到曼谷，寻味人间烟火气',
-        imageSrc: '../img/index_midj_artists.jpg',
-        url: ''
-    },
-    {
-        title: '《天文爱好者》2025：宇宙奥秘',
-        description: '最新星系发现与深空摄影技术',
-        imageSrc: '../img/chatbg.png',
-        url: ''
-    },
-    {
-        title: '《时尚盛典》：2025巴黎时装周回顾',
-        description: '高定设计师与可持续时尚新潮流',
-        imageSrc: '../img/index_midj_artists.jpg',
-        url: ''
-    },
-    {
-        title: '《动物百科》：深海生物探秘',
-        description: '镜头下的一千种海洋奇观',
-        imageSrc: '../img/chatbg.png',
+        description: '待发布',
+        imageSrc: '../img/pictorial-cover/2025-1.jpg',
         url: ''
     }
 ]
