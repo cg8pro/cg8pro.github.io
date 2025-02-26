d650dad01614cfe33203a14bcac65272945500c4
Dva R <cg8>
2025-02-26 22:54:48 +0800

    修复下载和画报列表的移动端问题

--- js/publications.js
+++ js/publications.js
@@ -3,6 +3,6 @@ var publications = [
         title: '2025-1',
         description: '待发布',
         imageSrc: '../img/pictorial-cover/2025-1.jpg',
-        url: ''
+        url: '/pictorial?'
     }
 ]
