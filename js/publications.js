815d819612ef185e69f542b808d1a9b6bb4a1d94
Dva R <cg8>
2025-03-10 00:56:34 +0800

    调整画报1期介绍

--- js/publications.js
+++ js/publications.js
@@ -1,7 +1,7 @@
 var publications = [
     {
         title: '2025-1',
-        description: '【SD灰阶点色技巧】【Mdjourney 玉足街头涂鸦】',
+        description: '【SD全息线雕】【Mdjourney 玉足街头涂鸦】',
         imageSrc: '../img/pictorial-cover/6e983f92.img.jpg',
         url: '/zh/pictorial?6e983f92ded07ed9dd3f13027d0d7d31bce9ced79d5f7959f5181b3682e6ded54e16f4b1d6e33606715b3c1c20f9a703e38d19720bae0cdd1144811755be6837'
     },
