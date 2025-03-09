77608fcb37d481d83465e07b4a57eb79bc577c82
Dva R <cg8>
2025-03-09 19:02:37 +0800

    更新画报1期串

--- js/publications.js
+++ js/publications.js
@@ -3,7 +3,7 @@ var publications = [
         title: '2025-1',
         description: '【SD灰阶点色技巧】【Mdjourney 玉足街头涂鸦】',
         imageSrc: '../img/pictorial-cover/6e983f92.img.jpg',
-        url: '/zh/pictorial?6e983f92ded07ed9dd3f13027d0d7d31bce9ced79d5f7959f5181b3682e6ded54e16f4b1d6e33606715b3c1c20f9a703e38d197255be6837'
+        url: '/zh/pictorial?6e983f92ded07ed9dd3f13027d0d7d31bce9ced79d5f7959f5181b3682e6ded54e16f4b1d6e33606715b3c1c20f9a703e38d19720bae0cdd1144811755be6837'
     },
     {
         title: '2025-2',
